308bc0cbbd34073627a729b57efd725c6aee8223
Daeseong Kim <daeseongkim05@gmail.com>
2024-12-28 23:20:07 +0900

    fix: movieResponseDto 응답 전체로 수정

--- lib/data/dto/movie_response_dto.dart
+++ lib/data/dto/movie_response_dto.dart
@@ -1,4 +1,60 @@
 class MovieResponseDto {
+  Dates dates;
+  int page;
+  List<Result> results;
+  int totalPages;
+  int totalResults;
+
+  MovieResponseDto({
+    required this.dates,
+    required this.page,
+    required this.results,
+    required this.totalPages,
+    required this.totalResults,
+  });
+
+  factory MovieResponseDto.fromJson(Map<String, dynamic> json) =>
+      MovieResponseDto(
+        dates: Dates.fromJson(json["dates"]),
+        page: json["page"],
+        results:
+            List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+        totalPages: json["total_pages"],
+        totalResults: json["total_results"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "dates": dates.toJson(),
+        "page": page,
+        "results": List<dynamic>.from(results.map((x) => x.toJson())),
+        "total_pages": totalPages,
+        "total_results": totalResults,
+      };
+}
+
+class Dates {
+  DateTime maximum;
+  DateTime minimum;
+
+  Dates({
+    required this.maximum,
+    required this.minimum,
+  });
+
+  factory Dates.fromJson(Map<String, dynamic> json) => Dates(
+        maximum: DateTime.parse(json["maximum"]),
+        minimum: DateTime.parse(json["minimum"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "maximum":
+            "${maximum.year.toString().padLeft(4, '0')}-${maximum.month.toString().padLeft(2, '0')}-${maximum.day.toString().padLeft(2, '0')}",
+        "minimum":
+            "${minimum.year.toString().padLeft(4, '0')}-${minimum.month.toString().padLeft(2, '0')}-${minimum.day.toString().padLeft(2, '0')}",
+      };
+}
+
+class Result {
   bool adult;
   String backdropPath;
   List<int> genreIds;
@@ -14,7 +70,7 @@ class MovieResponseDto {
   double voteAverage;
   int voteCount;
 
-  MovieResponseDto({
+  Result({
     required this.adult,
     required this.backdropPath,
     required this.genreIds,
@@ -31,8 +87,7 @@ class MovieResponseDto {
     required this.voteCount,
   });
 
-  factory MovieResponseDto.fromJson(Map<String, dynamic> json) =>
-      MovieResponseDto(
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
         adult: json["adult"],
         backdropPath: json["backdrop_path"],
         genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
--- test/data/dto/movie_response_dto_test.dart
+++ test/data/dto/movie_response_dto_test.dart
@@ -9,30 +9,41 @@ void main() {
     () {
       const sampleJsonString = """
 {
-  "adult": false,
-  "backdrop_path": "/cjEcqdRdPQJhYre3HUAc5538Gk8.jpg",
-  "genre_ids": [
-    28,
-    14,
-    35
+  "dates": {
+    "maximum": "2025-01-01",
+    "minimum": "2024-11-20"
+  },
+  "page": 1,
+  "results": [
+    {
+      "adult": false,
+      "backdrop_path": "/cjEcqdRdPQJhYre3HUAc5538Gk8.jpg",
+      "genre_ids": [
+        28,
+        14,
+        35
+      ],
+      "id": 845781,
+      "original_language": "en",
+      "original_title": "Red One",
+      "overview": "크리스마스 D-1, 철통같은 보안을 뚫고 코드명 '레드 원' 산타클로스가 납치되고 크리스마스가 사라질 위기에 처했다! '레드 원'을 찾기 위해 사령관 '칼럼 드리프트'는 산타클로스 따위는 없다고 믿는 현상금 사냥꾼 '잭 오말리'와 협력하기로 한다. 시작부터 삐그덕 거리는 이들 앞에 크리스마스의 존재를 위협하는 위험천만한 적들이 나타나는데…",
+      "popularity": 5083.147,
+      "poster_path": "/4zNUNhVpSqFggxqvdSXDRzy1QwE.jpg",
+      "release_date": "2024-10-31",
+      "title": "레드 원",
+      "video": false,
+      "vote_average": 7.026,
+      "vote_count": 1480
+    }
   ],
-  "id": 845781,
-  "original_language": "en",
-  "original_title": "Red One",
-  "overview": "크리스마스 D-1, 철통같은 보안을 뚫고 코드명 '레드 원' 산타클로스가 납치되고 크리스마스가 사라질 위기에 처했다! '레드 원'을 찾기 위해 사령관 '칼럼 드리프트'는 산타클로스 따위는 없다고 믿는 현상금 사냥꾼 '잭 오말리'와 협력하기로 한다. 시작부터 삐그덕 거리는 이들 앞에 크리스마스의 존재를 위협하는 위험천만한 적들이 나타나는데…",
-  "popularity": 5083.147,
-  "poster_path": "/4zNUNhVpSqFggxqvdSXDRzy1QwE.jpg",
-  "release_date": "2024-10-31",
-  "title": "레드 원",
-  "video": false,
-  "vote_average": 7.026,
-  "vote_count": 1480
+  "total_pages": 238,
+  "total_results": 4758
 }
 """;
       final movie = MovieResponseDto.fromJson(jsonDecode(sampleJsonString));
-      expect(movie.title, "레드 원");
-      expect(movie.originalTitle, "Red One");
-      expect(movie.popularity, 5083.147);
+      expect(movie.results[0].title, "레드 원");
+      expect(movie.results[0].originalTitle, "Red One");
+      expect(movie.results[0].popularity, 5083.147);
     },
   );
 }
