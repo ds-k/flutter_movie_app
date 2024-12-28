b0f4832b75f0cff1eebdd3bbe216125f00cc74a7
Daeseong Kim <daeseongkim05@gmail.com>
2024-12-29 05:34:10 +0900

    feat: movieRepositoryImpl 작성

---
+++ lib/data/repository/movie_repository_impl.dart
@@ -0,0 +1,117 @@
+import 'package:flutter_movie_app/data/data_source/movie_data_source.dart';
+import 'package:flutter_movie_app/domain/entity/movie.dart';
+import 'package:flutter_movie_app/domain/entity/movie_detail.dart';
+import 'package:flutter_movie_app/domain/repository/movie_repository.dart';
+
+class MovieRepositoryImpl implements MovieRepository {
+  MovieRepositoryImpl(this._movieDataSource);
+  final MovieDataSource _movieDataSource;
+
+  @override
+  Future<MovieDetail?> fetchMovieDetail(int id) async {
+    try {
+      final result = await _movieDataSource.fetchMovieDetail(id);
+      {
+        if (result != null) {
+          return MovieDetail(
+            id: result.id,
+            title: result.title,
+            overview: result.overview,
+            releaseDate: result.releaseDate,
+            voteAverage: result.voteAverage,
+            voteCount: result.voteCount,
+            budget: result.budget,
+            genres: result.genres.map((genre) => genre.name).toList(),
+            productionCompanyLogos: result.productionCompanies
+                .map((company) => company.logoPath ?? "")
+                .toList(),
+            popularity: result.popularity,
+            revenue: result.revenue,
+            runtime: result.runtime,
+            tagline: result.tagline,
+          );
+        }
+      }
+      return null;
+    } on Exception catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  @override
+  Future<List<Movie>?> fetchNowPlayingMovies() async {
+    try {
+      final result = await _movieDataSource.fetchNowPlayingMovies();
+      if (result != null) {
+        return result.results
+            .map((movie) => Movie(
+                  id: movie.id,
+                  posterPath: movie.posterPath,
+                ))
+            .toList();
+      }
+      return null;
+    } on Exception catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  @override
+  Future<List<Movie>?> fetchPopularMovies() async {
+    try {
+      final result = await _movieDataSource.fetchPopularMovies();
+      if (result != null) {
+        return result.results
+            .map((movie) => Movie(
+                  id: movie.id,
+                  posterPath: movie.posterPath,
+                ))
+            .toList();
+      }
+      return null;
+    } on Exception catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  @override
+  Future<List<Movie>?> fetchTopRatedMovies() async {
+    try {
+      final result = await _movieDataSource.fetchTopRatedMovies();
+      if (result != null) {
+        return result.results
+            .map((movie) => Movie(
+                  id: movie.id,
+                  posterPath: movie.posterPath,
+                ))
+            .toList();
+      }
+      return null;
+    } on Exception catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  @override
+  Future<List<Movie>?> fetchUpcomingMovies() async {
+    try {
+      final result = await _movieDataSource.fetchUpcomingMovies();
+      if (result != null) {
+        return result.results
+            .map((movie) => Movie(
+                  id: movie.id,
+                  posterPath: movie.posterPath,
+                ))
+            .toList();
+      }
+      return null;
+    } on Exception catch (e) {
+      print(e);
+      return null;
+    }
+  }
+}
