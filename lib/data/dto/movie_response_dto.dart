a72e74fb2bf8af5b1033c34b523134e251fbbf34
Daeseong Kim <daeseongkim05@gmail.com>
2024-12-30 16:28:35 +0900

    fix: test case를 확인해서 올바른 타입의 데이터가 오는지 확인하고 잘못된 내용 수정

--- lib/data/dto/movie_response_dto.dart
+++ lib/data/dto/movie_response_dto.dart
@@ -1,6 +1,6 @@
 class MovieResponseDto {
-  Dates dates;
-  int page;
+  Dates? dates;
+  int? page;
   List<Result> results;
   int totalPages;
   int totalResults;
@@ -13,18 +13,19 @@ class MovieResponseDto {
     required this.totalResults,
   });
 
-  factory MovieResponseDto.fromJson(Map<String, dynamic> json) =>
-      MovieResponseDto(
-        dates: Dates.fromJson(json["dates"]),
-        page: json["page"],
-        results:
-            List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
-        totalPages: json["total_pages"],
-        totalResults: json["total_results"],
-      );
+  factory MovieResponseDto.fromJson(Map<String, dynamic> json) {
+    return MovieResponseDto(
+      dates: json["dates"] != null ? Dates.fromJson(json["dates"]) : null,
+      page: json["page"],
+      results:
+          List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+      totalPages: json["total_pages"],
+      totalResults: json["total_results"],
+    );
+  }
 
   Map<String, dynamic> toJson() => {
-        "dates": dates.toJson(),
+        "dates": dates?.toJson(),
         "page": page,
         "results": List<dynamic>.from(results.map((x) => x.toJson())),
         "total_pages": totalPages,
@@ -56,7 +57,7 @@ class Dates {
 
 class Result {
   bool adult;
-  String backdropPath;
+  String? backdropPath;
   List<int> genreIds;
   int id;
   String originalLanguage;
@@ -87,22 +88,24 @@ class Result {
     required this.voteCount,
   });
 
-  factory Result.fromJson(Map<String, dynamic> json) => Result(
-        adult: json["adult"],
-        backdropPath: json["backdrop_path"],
-        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
-        id: json["id"],
-        originalLanguage: json["original_language"],
-        originalTitle: json["original_title"],
-        overview: json["overview"],
-        popularity: json["popularity"]?.toDouble(),
-        posterPath: json["poster_path"],
-        releaseDate: DateTime.parse(json["release_date"]),
-        title: json["title"],
-        video: json["video"],
-        voteAverage: json["vote_average"]?.toDouble(),
-        voteCount: json["vote_count"],
-      );
+  factory Result.fromJson(Map<String, dynamic> json) {
+    return Result(
+      adult: json["adult"],
+      backdropPath: json["backdrop_path"],
+      genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+      id: json["id"],
+      originalLanguage: json["original_language"],
+      originalTitle: json["original_title"],
+      overview: json["overview"],
+      popularity: json["popularity"]?.toDouble(),
+      posterPath: json["poster_path"],
+      releaseDate: DateTime.parse(json["release_date"]),
+      title: json["title"],
+      video: json["video"],
+      voteAverage: json["vote_average"]?.toDouble(),
+      voteCount: json["vote_count"],
+    );
+  }
 
   Map<String, dynamic> toJson() => {
         "adult": adult,
