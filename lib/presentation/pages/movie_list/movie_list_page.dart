3a47be06f6172327c848a4e21af8ed32423c0819
Daeseong Kim <daeseongkim05@gmail.com>
2025-01-01 18:18:42 +0900

    feat: MovieListPage UI 적용

--- lib/presentation/pages/movie_list/movie_list_page.dart
+++ lib/presentation/pages/movie_list/movie_list_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movie_app/presentation/pages/movie_detail/movie_detail_page.dart';
 import 'package:flutter_movie_app/presentation/pages/movie_list/movie_list_view_model.dart';
+import 'package:flutter_movie_app/presentation/pages/movie_list/widgets/category_title.dart';
+import 'package:flutter_movie_app/presentation/pages/movie_list/widgets/movie_list.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shimmer/shimmer.dart';
 
 class MovieListPage extends ConsumerWidget {
   const MovieListPage({super.key});
@@ -12,156 +13,98 @@ class MovieListPage extends ConsumerWidget {
     final movieListState = ref.watch(movieListViewModelProvider);
 
     return Scaffold(
-      body: movieListState.when(
-        data: (movieListState) {
-          final nowPlayingMovies = movieListState.nowPlayingMoviesList ?? [];
-          final popularMovies = movieListState.popularMoviesList ?? [];
-          final topRatedMovies = movieListState.topRatedMoviesList ?? [];
-          final upcomingMovies = movieListState.upcomingMoviesList ?? [];
-
-          return SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "현재 상영중",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                movieList(
-                  context: context,
-                  movies: nowPlayingMovies,
-                  title: "현재 상영중",
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "인기순",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                movieList(
-                  context: context,
-                  movies: popularMovies,
-                  title: "인기순",
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "평점 높은순",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                movieList(
-                  context: context,
-                  movies: topRatedMovies,
-                  title: "평점 높은순",
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "개봉 예정",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                movieList(
-                  context: context,
-                  movies: upcomingMovies,
-                  title: "개봉 예정",
-                ),
-              ],
-            ),
-          );
-        },
-        loading: () {
-          return const Center(child: CircularProgressIndicator());
-        },
-        error: (error, stackTrace) {
-          return Center(child: Text("Error: $error / on : $stackTrace"));
-        },
-      ),
-    );
-  }
-
-  Widget movieList({
-    required BuildContext context,
-    required List movies,
-    required String title,
-  }) {
-    return SizedBox(
-      height: 200,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: movies.length,
-        itemBuilder: (context, index) {
-          final movie = movies[index];
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MovieDetailPage(movie: movie),
-                  ),
-                );
+      body: SafeArea(
+        child: movieListState.when(
+          data: (movieListState) {
+            final categories = [
+              {
+                'title': "가장 인기있는",
+                'movies': movieListState.popularMoviesList ?? []
               },
-              child: ShimmerLoadingImage(
-                imageUrl: "http://image.tmdb.org/t/p/w500/${movie.posterPath}",
-                width: 120,
-                height: 180,
-              ),
-            ),
-          );
-        },
+              {
+                'title': "현재 상영중",
+                'movies': movieListState.nowPlayingMoviesList ?? []
+              },
+              {
+                'title': "인기 순",
+                'movies': movieListState.popularMoviesList ?? []
+              },
+              {
+                'title': "평점 높은 순",
+                'movies': movieListState.topRatedMoviesList ?? []
+              },
+              {
+                'title': "개봉 예정",
+                'movies': movieListState.upcomingMoviesList ?? []
+              },
+            ];
+
+            return ListView.builder(
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                final category = categories[index];
+                final movies = category['movies'] as List;
+
+                if (index == 0) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CategoryTitle(title: category['title'] as String),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  MovieDetailPage(movie: movies[0]),
+                            ),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.network(
+                              "https://image.tmdb.org/t/p/w500${movies[0].posterPath}",
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                } else if (index == 2) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CategoryTitle(title: category['title'] as String),
+                      MovieList(
+                        movies: movies,
+                        isPopularList: true,
+                      ),
+                    ],
+                  );
+                } else {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CategoryTitle(title: category['title'] as String),
+                      MovieList(movies: movies),
+                    ],
+                  );
+                }
+              },
+            );
+          },
+          loading: () {
+            return const Center(child: CircularProgressIndicator());
+          },
+          error: (error, stackTrace) {
+            return Center(child: Text("Error: $error / on : $stackTrace"));
+          },
+        ), // Add your child widget here
       ),
     );
-  }
-}
-
-class ShimmerLoadingImage extends StatelessWidget {
-  final String imageUrl;
-  final double width;
-  final double height;
-
-  const ShimmerLoadingImage({
-    required this.imageUrl,
-    required this.width,
-    required this.height,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _precacheImage(context, imageUrl),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Image.network(
-            imageUrl,
-            width: width,
-            height: height,
-            fit: BoxFit.cover,
-          );
-        } else {
-          return Shimmer.fromColors(
-            baseColor: Colors.grey[300]!,
-            highlightColor: Colors.grey[100]!,
-            child: Container(
-              width: width,
-              height: height,
-              color: Colors.grey,
-            ),
-          );
-        }
-      },
-    );
-  }
-
-  Future<void> _precacheImage(BuildContext context, String url) async {
-    final image = NetworkImage(url);
-    await precacheImage(image, context);
   }
 }
